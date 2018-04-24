4f5266ff2910022f49098be8e324b4d1aaa4f611
Phuong <le.phuong.929@gmail.com>
2018-04-23 21:11:00 -0400

    updated orm

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('bacon cheeseburger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('rodeo', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('carolina', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('animal style', true);
+INSERT INTO burgers (burger_name) VALUES ('Five Guys');
